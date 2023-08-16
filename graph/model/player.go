aad07738c97b0453e883f4b742f1c7aa6dbe65e1
Drew Harris <harrisd@smu.edu>
2023-08-16 14:23:49 -0500

    Fix struct tag

--- graph/model/player.go
+++ graph/model/player.go
@@ -6,5 +6,5 @@ type Player struct {
 	ID        string    `json:"id" gorm:"primaryKey;not null;size:255"`
 	Name      string    `json:"name" gorm:"not null"`
 	CreatedAt time.Time `json:"createdAt" gorm:"not null"`
-	Logs      []Log     `json:"logs" gorm:"foreignKey:PlayerID;references:ID`
+	Logs      []Log     `json:"logs" gorm:"foreignKey:PlayerID;references:ID"`
 }
