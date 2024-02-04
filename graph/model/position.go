c2a12831d9422dcab46ab2d978f0233156779cc6
Drew Harris <harrisd@smu.edu>
2024-02-04 15:57:24 -0600

    Add primary key to position

--- graph/model/position.go
+++ graph/model/position.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Position struct {
+	ID             string         `json:"id" gorm:"primaryKey;not null"`
 	Player         *Player        `json:"player" gorm:"not null"`
 	PlayerID       string         `json:"playerId" gorm:"not null"`
-	ID             string         `json:"id" gorm:"not null"`
 	ObjectiveID    string         `json:"objectiveID" gorm:"not null"`
 	Unit           string         `json:"unit" gorm:"not null"`
 	SavedAt        time.Time      `json:"savedAt" gorm:"not null;autoCreateTime"`
